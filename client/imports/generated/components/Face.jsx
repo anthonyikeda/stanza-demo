55a5769e8e22987dba8479e8fc5f9735593602d8
Chun Yang <yang2007chun@gmail.com>
2016-08-09 09:34:49 -0500

    update to use bare bone webflow project

--- client/imports/generated/components/Face.jsx
+++ client/imports/generated/components/Face.jsx
@@ -7,7 +7,6 @@ class Face extends React.Component {
       <div>
         <div className='background'>
           <h1>face :) hidden</h1>
-          <h2 data-component-redux-value='name'>dynamic value</h2>
         </div>
       </div>
       );
--- client/imports/generated/components/Login.jsx
+++ client/imports/generated/components/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import LoginForm from './LoginForm';
 import { connect as reduxConnect } from 'react-redux';
 import action from '../action-creators/index';
 import custom from '../../custom/index';
@@ -10,7 +9,40 @@ class Login extends React.Component {
       <div>
         <div className='form-background'>
           <div className='w-form form'>
-            <LoginForm></LoginForm>
+            <form data-name='Email Form'
+              id='email-form'
+              name='email-form'
+              onSubmit={this.props['action.login.submitEmailForm']}>
+              <label htmlFor='name'>
+                NAME
+              </label>
+              <input data-name='Name'
+                id='name'
+                name='name'
+                placeholder='Enter your name'
+                type='text'
+                className='w-input'
+                maxLength='256'
+                value={this.props['state.login.name']}
+                onChange={this.props['action.login.changeName']} />
+              <label htmlFor='email'>
+                EMAIL
+              </label>
+              <input data-name='Email'
+                id='email'
+                name='email'
+                placeholder='Enter your email address'
+                required='required'
+                type='email'
+                className='w-input'
+                maxLength='256'
+                value={this.props['state.login.email']}
+                onChange={this.props['action.login.changeEmail']} />
+              <input data-wait='Please wait...'
+                type='submit'
+                value='Submit'
+                className='w-button button' />
+            </form>
             <div className='w-form-done'>
               <div>
                 Thank you! Your submission has been received!
